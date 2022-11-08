6839551e99b3adb0f2c723dc5d8795146a0d2858
TarekBellalouna <tarek.bellalouna@esprit.tn>
2022-11-08 16:58:40 +0100

    fixing

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8
-ADD http://192.168.1.22:8081/repository/maven-releases/tn/esprit/rh/achat/1.0/achat-1.0.jar achat-1.0.jar
+FROM openjdk:8-jdk-alpine
+ADD target/achattarek.jar achattarek.jar
 EXPOSE 8089
-ENTRYPOINT ["java", "-jar", "/achat-1.0.jar"]
+ENTRYPOINT ["java", "-jar", "achattarek.jar"]
--- src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceTest.java
@@ -17,18 +17,19 @@ public class ProduitServiceTest {
 
     @Autowired
     IProduitService ps;
-
+    Produit produit = new Produit("p", "pc" , 12 ,new Date() );
     @Order(1)
     @Test
     void Add(){
-        log.info("Add Produit: "+ps.addProduit(new Produit(2L,"produit1", "console" , 500 ,new Date() )).toString());
+        Produit produitAdded =  ps.addProduit(produit);
+        Assertions.assertEquals(produitAdded.getCodeProduit(), produitAdded.getCodeProduit());
     }
 
     @Test
     @Order(2)
     public void testRetrieveAllUsers() {
         List<Produit> listProduits = ps.retrieveAllProduits();
-        Assertions.assertEquals(0, listProduits.size());
+        Assertions.assertEquals(1, listProduits.size());
     }
     
 
